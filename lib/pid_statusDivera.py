10ca32c53dd2c9c1fa34a26e3c5bf2cc95c94deb
tobst96 <52480593+tobst96@users.noreply.github.com>
2021-11-19 14:05:43 +0100

    Altlast entfernt

--- lib/pid_statusDivera.py
+++ lib/pid_statusDivera.py
@@ -12,7 +12,7 @@ LOGGER = logging.getLogger('>>>main<<<')
 chromalog.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s - %(message)s')
 fh = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s - %(filename)s')
 LOGGER.setLevel(logging.DEBUG)
-file = ("/var/api/logging.log")
+file = ("/var/StatusClient/StatusAPI/logging.log")
 fh = logging.FileHandler(file, encoding = "UTF-8")
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s - %(filename)s - %(funcName)s')
@@ -45,26 +45,10 @@ try:
         LOGGER.debug("[" + str(os.getpid()) + "] " + "[Divera] " + token[0:10] + " " + msg)  
 
         
-
-        # You can generate a Token from the "Tokens Tab" in the UI
-        token = "jFG4vCStSySFlQA9hTrjDfdHZJAD93qn14iCGrd2g75jVDJYW5gMaTg7uhGtlie0Kril0Xuj3oU3g8ScEXdS-g=="
-        org = "Obst"
-        bucket = "Status"
-
-        client = InfluxDBClient(url="http://192.168.2.19:8086", token=token)
-
-
-
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-
-
-        p = Point("Output:Divera").tag("token", str(tokendb)).tag("status", str(status)).tag("name", str(name)).field("r_statuscode", int(r.status_code))
-        write_api.write(bucket=bucket, org=org, record=p)
-
     except Exception as ex:
-        LOGGER.critical("[" + str(os.getpid()) + "] " + "send divera " + str(ex)) 
+        LOGGER.critical("[" + str(os.getpid()) + "] pid_StatusDivera" + "send divera " + str(ex)) 
     
     
 
 except Exception as ex:
-    LOGGER.error("[" + str(os.getpid()) + "] " + str(ex))
+    LOGGER.error("[" + str(os.getpid()) + "] pid_StatusDivera" + str(ex))
--- lib/pid_statusfireboard.py
+++ lib/pid_statusfireboard.py
@@ -19,7 +19,7 @@ LOGGER = logging.getLogger('>>>main<<<')
 chromalog.basicConfig(level=logging.INFO, format='[%(levelname)s] %(asctime)s - %(message)s')
 fh = logging.Formatter('[%(levelname)s] %(asctime)s - %(message)s - %(filename)s')
 LOGGER.setLevel(logging.DEBUG)
-file = ("/var/api/logging.log")
+file = ("/var/StatusClient/StatusAPI/logging.log")
 fh = logging.FileHandler(file, encoding = "UTF-8")
 fh.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s.%(msecs)03d - %(name)s - %(levelname)s - %(message)s - %(filename)s - %(funcName)s')
@@ -77,21 +77,6 @@ def main(status, issi, name, token):
         time_elapsed = datetime.now() - start_time 
         LOGGER.debug("[" + str(os.getpid()) + "]" + '[Dauer] Fireboard senden: {}'.format(time_elapsed))
         
-
-        # You can generate a Token from the "Tokens Tab" in the UI
-        token = "jFG4vCStSySFlQA9hTrjDfdHZJAD93qn14iCGrd2g75jVDJYW5gMaTg7uhGtlie0Kril0Xuj3oU3g8ScEXdS-g=="
-        org = "Obst"
-        bucket = "Status"
-
-        client = InfluxDBClient(url="http://192.168.2.19:8086", token=token)
-
-
-
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-
-        p = Point("Output:Fireboard").tag("issi", str(issi)).field("status", int(status)).tag("name", str(name).tag("token", str(args.api_key)))
-        write_api.write(bucket=bucket, org=org, record=p)
-        return 0
 
     
 
